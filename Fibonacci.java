b92f610abc005252a1d595f17c1e8fe7515ba307
Borhan Rabbani <borhanuddinse28@gmail.com>
2022-06-17 21:51:28 +0600

    Update Fibonacci.java
    optimized Fibonacchi solution

--- Fibonacci.java
+++ Fibonacci.java
@@ -30,11 +30,30 @@ public class Fibonacci {
 		memo.put(n, result);
 		return result;
 	}
+	
+	// Fibonacci optimized solution Time: O(n), Space: O(1)
+	
+	public int fib_optimized(int n) {
+		if(n<=1) {
+			return n;
+		}
+		
+		int a=0, b=1;
+		
+		for(int i=2; i<=n; i++) {
+			int sum = a+b;
+			a=b;
+			b=sum;
+		}
+		
+		return b;
+	}
 
 	public static void main(String[] args) {
 
 		Fibonacci fibo = new Fibonacci();
 		System.out.println(fibo.fib_memo(10));
+		System.out.println(fibo.fib_optimized(67));
 	}
 
 }
